c7b0d4654fa697691d9ca28d757d86d45cc91d79
Crowdbotics <team@crowdbotics.com>
2023-09-25 02:11:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "moneykebet-43549",
+  url: "https://moneykebet-43549.botics.co",
+  api: "https://moneykebet-43549.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
